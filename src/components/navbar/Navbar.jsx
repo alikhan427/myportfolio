23a09e655a18a8ceda9860738ffa928f41c73fd1
muhammadasad22 <alikhan03009050164@gmail.com>
2026-07-03 20:09:29 +0500

    make some changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,60 +4,52 @@ import { HiMenu, HiX } from 'react-icons/hi';
 import './Navbar.css';
 
 const Navbar = () => {
-
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
-
-      {/* LOGO */}
       <div className="logo">
-        <Link
-          to="/"
-          style={{ textDecoration: 'none', color: 'inherit' }}
-        >
-          <span>PortFolio</span>
+        <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <span>MA</span>
         </Link>
       </div>
 
-      {/* MENU ICON */}
-      <div
-        className="menu-icon"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
         {menuOpen ? <HiX /> : <HiMenu />}
       </div>
 
-      {/* NAV LINKS */}
       <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li>
-          <Link to="/about" onClick={() => setMenuOpen(false)}>
-            About me
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
           </Link>
         </li>
-
+        <li>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
+        </li>
         <li>
           <Link to="/skills" onClick={() => setMenuOpen(false)}>
             Skills
           </Link>
         </li>
-
         <li>
-          <Link to="/portfolio" onClick={() => setMenuOpen(false)}>
-            Portfolio
+          <Link to="/projects" onClick={() => setMenuOpen(false)}>
+            Projects
           </Link>
         </li>
-
+        <li>
+          <Link to="/testimonials" onClick={() => setMenuOpen(false)}>
+            Testimonials
+          </Link>
+        </li>
         <li>
           <Link to="/contact" onClick={() => setMenuOpen(false)}>
-            <button className="contact-btn">
-              CONTACT ME
-            </button>
+            <button className="contact-btn">Contact</button>
           </Link>
         </li>
-
       </ul>
-
     </nav>
   );
 };
